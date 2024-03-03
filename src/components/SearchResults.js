9b9ff37d127d05084a5e27b4bc601ebe402f7ae1
Thasnima Begum <thasnima@hotmail.co.uk>
2024-03-03 16:32:20 +0000

    styling added

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,15 +3,15 @@ import "../styles/searchResults.css";
 
 export default function SearchResults({ results }) {
 	if (!results.length) {
-		return <p>Nothing here</p>;
+		return <p className="title">No images</p>;
 	} else {
 		const resultImages = results.map((image) => {
 			return <img className="card-image" src={image} alt="" />;
 		});
 		return (
 			<>
-				<p>Search Results</p>
-				{resultImages}
+				<p className="title">Search Results</p>
+				<div className="card-image-container">{resultImages}</div>
 			</>
 		);
 	}
